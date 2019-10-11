22c88a94359d77ce6dac4268700e367df20ff424
Andre Gurgel <andreggurgelufrn@gmail.com>
2019-10-11 14:21:20 -0300

    Projeto contador de pessoas.

--- contadorDePessoasV2/contadorDePessoasV2.ino
+++ contadorDePessoasV2/contadorDePessoasV2.ino
@@ -1,3 +1,5 @@
+unsigned long time = 0; // Zerar time
+
 #include <Ultrasonic.h> // Sensor Ultrassom
 #include <LiquidCrystal_I2C.h> // Biblioteca do LCD
 #include <Wire.h> // Biblioteca para o funcionamento do LCD
@@ -5,8 +7,6 @@
 //Define os pinos para o trigger e echo
 #define trigger 7
 #define echo 6
-// #define led 3
-// #define sensor 2
 
 Ultrasonic ultrassom(7,6);
 LiquidCrystal_I2C Display(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
@@ -15,14 +15,9 @@ byte a[8]= {B00110,B01001,B00110,B00000,B00000,B00000,B00000,B00000,}; // Array 
 
 int contador = 0; // Contar numero de pessoas
 long distancia;
-// boolean valorS;
 
 void setup() {
   Serial.begin(9600);
-
-  // Led e sensor de movmento
-  // pinMode(led, HIGH);
-  // pinMode(sensor, INPUT);
   
   // Display
   Display.begin(16,2);
@@ -32,8 +27,6 @@ void setup() {
 }
 
 void loop() {
-  //Pega valor do sensor
-  // valorS = digitalRead(sensor);
   
   // Escreve no Display
   Display.setBacklight(HIGH);
@@ -43,15 +36,19 @@ void loop() {
   Display.print(" de pessoas:");
   Display.setCursor(7,1);
   Display.print(contador);
-  
+
   // Sensor Ultrassom
   distancia = ultrassom.Ranging(CM);// ultrassom.Ranging(CM) retorna a distancia em centímetros(CM) ou polegadas(INC)
-  //Serial.print(distancia); //imprime o valor da variável distancia
-  //Serial.println("cm");
+  Serial.print(distancia); //imprime o valor da variável distancia
+  Serial.println("cm");
   
-
-  if(distancia >= 35 && distancia <= 62){
-      contador++;
-      delay(1000);
+  
+  if(distancia >= 25 && distancia <= 62){
+   while((millis() - time >= 1000) && (distancia >= 25 && distancia <= 62)){
+    contador++;
+    // Inicia a millis
+    
+  } 
   }
+  time = millis();
 }
